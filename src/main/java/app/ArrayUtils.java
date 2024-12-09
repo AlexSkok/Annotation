72705178b3e6f3cdcb52857ba70f085bb7c59782
Alex <olexiyskok@gmail.com>
2024-12-09 19:59:18 +0200

    to push

--- src/main/java/app/ArrayUtils.java
+++ src/main/java/app/ArrayUtils.java
@@ -4,7 +4,7 @@ import app.annotation.Author;
 import app.annotation.MethodInfo;
 
 public class ArrayUtils {
-    @MethodInfo(title = "Method in class ArrayUtil", description = "For annotation hometask")
+    @MethodInfo(title = "Method in class ArrayUtil", description = "For annotation hometask", returnType = "void")
     @Author(author = "Alex Skok")
     public void annotationMethod(){
         System.out.println("Test nethod for annotation hometask");
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,20 @@
 package app;
 
+import app.annotation.Author;
+import app.annotation.MethodInfo;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            MethodInfo methodInfo = ArrayUtils.class.getDeclaredMethod("annotationMethod").getAnnotation(MethodInfo.class);
+            Author author = ArrayUtils.class.getDeclaredMethod("annotationMethod").getAnnotation(Author.class);
+            System.out.println("Назва методу " + methodInfo.title());
+            System.out.println("Опис методу " + methodInfo.description());
+            System.out.println("Type to return: " + methodInfo.returnType());
+            System.out.println("Return " + ArrayUtils.class.getDeclaredMethod("annotationMethod").getReturnType());
+            System.out.println("Автор методу " + author.author());
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
